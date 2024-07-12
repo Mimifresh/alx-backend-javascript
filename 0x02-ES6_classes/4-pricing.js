37ba638348a6b5d39a80e5a4d7b40ea3953440b5
Mimifresh <mimigolld@gmail.com>
2024-07-12 01:32:31 +0100

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,11 +1,30 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+}export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
   }
 
-  cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(n) {
+    this._amount = n;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(n) {
+    this._currency = n;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }
